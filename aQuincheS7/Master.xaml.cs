9639ce64ab825a3482a7e1dc193f649675fdd3ef
HOME <HOME@DESKTOP-L6UBTV3>
2023-07-10 18:47:45 -0500

    Agregar archivos de proyecto.

---
+++ aQuincheS7/Master.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace aQuincheS7
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Master : ContentPage
+    {
+        public Master()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInicio_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAcerca_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
